04cb6d6ef6833317ece397ff9230ae56249b0a8b
Chris Box <Chris.Box@MarkLogic.com>
2019-06-07 12:42:38 -0400

    Fixing outputs

--- modules/marklogic/modules/server-group/outputs.tf
+++ modules/marklogic/modules/server-group/outputs.tf
@@ -1,8 +1,8 @@
 
 output "eni_private_dns" {
-  value = var.enable ? [ for eni in aws_network_interface.managed_eni: eni.private_dns_name ] : null
+  value = [ for eni in aws_network_interface.managed_eni: eni.private_dns_name ]
 }
 
 output "eni_private_ips" {
-  value = var.enable ? [ for eni in aws_network_interface.managed_eni: eni.private_ip ] : null
+  value = [ for eni in aws_network_interface.managed_eni: eni.private_ip ]
 }
--- modules/marklogic/outputs.tf
+++ modules/marklogic/outputs.tf
@@ -11,35 +11,23 @@ output "internal_url" {
 output "managed_eni_private_ips" {
   value = [ for value in [
     module.server_group_0.eni_private_ips,
-//    module.server_group_1.eni_private_ips,
-//    module.server_group_2.eni_private_ips,
-//    module.server_group_3.eni_private_ips,
-//    module.server_group_4.eni_private_ips,
-//    module.server_group_5.eni_private_ips,
-//    module.server_group_6.eni_private_ips,
-//    module.server_group_7.eni_private_ips,
-//    module.server_group_8.eni_private_ips,
-//    module.server_group_9.eni_private_ips,
-//    module.server_group_10.eni_private_ips,
-//    module.server_group_11.eni_private_ips,
-  ]: value if value != null ]
+    module.server_group_1.eni_private_ips,
+    module.server_group_2.eni_private_ips,
+    module.server_group_3.eni_private_ips,
+    module.server_group_4.eni_private_ips,
+    module.server_group_5.eni_private_ips,
+  ]: value if length(value) > 0 ]
 }
 
 output "managed_eni_private_dns" {
   value = [ for value in [
     module.server_group_0.eni_private_dns,
-//    module.server_group_1.eni_private_dns,
-//    module.server_group_2.eni_private_dns,
-//    module.server_group_3.eni_private_dns,
-//    module.server_group_4.eni_private_dns,
-//    module.server_group_5.eni_private_dns,
-//    module.server_group_6.eni_private_dns,
-//    module.server_group_7.eni_private_dns,
-//    module.server_group_8.eni_private_dns,
-//    module.server_group_9.eni_private_dns,
-//    module.server_group_10.eni_private_dns,
-//    module.server_group_11.eni_private_dns,
-  ]: value if value != null ]
+    module.server_group_1.eni_private_dns,
+    module.server_group_2.eni_private_dns,
+    module.server_group_3.eni_private_dns,
+    module.server_group_4.eni_private_dns,
+    module.server_group_5.eni_private_dns,
+  ]: value if length(value) > 0 ]
 }
 
 output "instance_security_group_id" {
